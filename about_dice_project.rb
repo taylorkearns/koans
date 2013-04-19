daac8419e31fc372eccaa4b4877cfbef13ee5d71
Taylor Kearns <tkearns@gmail.com>
2013-04-18 22:22:27 -0400

    Stuck on dice project

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -1,10 +1,30 @@
 require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 
-# Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_accessor :values, :previous_values
+
+  def initialize
+    @values = []
+    @previous_values = nil
+  end
+
+  def roll(num)
+    values.clear
+    num.times do
+      values << roll_die
+    end
+  end
+
+  private
+
+  def roll_die
+    sides.sample
+  end
+
+  def sides
+    [1, 2, 3, 4, 5, 6]
+  end
+end
 
 class AboutDiceProject < EdgeCase::Koan
   def test_can_create_a_dice_set
